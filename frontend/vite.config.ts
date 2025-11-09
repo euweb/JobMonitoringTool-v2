1d71419947990d123787415d1427b1e19a242e94
Eugen Weber <eugen_w@yahoo.de>
2025-11-09 19:38:58 +0100

    fix absulute paths for assets

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./", // Use relative paths for assets
   resolve: {
     alias: {
       "@": path.resolve(process.cwd(), "src"),
